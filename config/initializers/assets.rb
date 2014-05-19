7f2cc75a7fcf2051c9defd388988bf4fc3b2aacf
cqzhangkang <cqzhangkang@163.com>
2014-05-20 02:53:24 +0800

    init

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( ie/html5shiv.js )
+Rails.application.config.assets.precompile += %w( ie/respond.min.js )
+Rails.application.config.assets.precompile += %w( ie/excanvas.js )
